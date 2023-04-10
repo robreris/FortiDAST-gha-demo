d9671c10e6a3fd2083abb3bd9b5639c8ae687428
Robert Reris <rreris@fortinet.com>
2023-04-10 12:39:59 -0400

    first commit

---
+++ scripts/fetch-configs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+root_level=$(git rev-parse --show-toplevel)
+while read -r line; do
+  line=($line)
+  file_resource=${line[0]}
+  if (( ${#line[@]} < 2 )); then path_location=$root_level; else path_location=$root_level/${line[1]}; fi
+  [[ -d $path_location ]] | mkdir -p $path_location
+  wget $file_resource -P $path_location 
+done < $root_level/.config_files
